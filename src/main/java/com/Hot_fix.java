6043f829aaf68a03448471b2679bd59ea76da1da
guoqing <13569180170@163.com>
2022-04-25 20:07:41 +0800

    hot-fix 3 commit

--- src/main/java/com/Hot_fix.java
+++ src/main/java/com/Hot_fix.java
@@ -4,5 +4,6 @@ public class Hot_fix {
     public static void main(String[] args) {
         System.out.println("hot fix1");
         System.out.println("hot fix2");
+        System.out.println("hot fix3");
     }
 }
